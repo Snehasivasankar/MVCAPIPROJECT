1a78fec174bb2407c17f8ccbedd6210e0f5340b7
Snehasivasankjar <snehasivasankar@gmail.com>
2025-11-20 23:33:42 +0530

    Add project files.

---
+++ API/APIController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCAPIPROJECT.API
+{
+    public class APIController : ApiController
+    {
+        Employee1Entities dbobj = new Employee1Entities();
+        // GET: api/API
+        [HttpGet]
+        [Route("api/API/getwebapitabs")]
+        public IHttpActionResult GetwebApiTabs()
+        {
+            return Json(dbobj.WebApiTabs.ToList());
+
+        }
+
+        // GET: api/API/5
+        [HttpGet]
+        [Route("api/API/getwebapitab/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            WebApiTab employee = dbobj.WebApiTabs.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        // POST: api/API
+        [HttpPost]
+        [Route("api/API/postwebapitab")]
+        public IHttpActionResult Post(WebApiTab webApiTab)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            dbobj.WebApiTabs.Add(webApiTab);
+            dbobj.SaveChanges();
+            return Ok(200);
+        }
+
+        // PUT: api/API/5
+        [HttpPut]
+        [Route("api/API/putwebapitab/{id}")]
+        public IHttpActionResult Put(int id, WebApiTab webApiTab)
+        {
+            dbobj.Entry(webApiTab).State = EntityState.Modified;
+
+            dbobj.SaveChanges();
+            return Ok(200);
+
+        }
+
+        // DELETE: api/API/5
+        [HttpDelete]
+        [Route("api/API/deletewebapitab/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            WebApiTab webApiTab = dbobj.WebApiTabs.Find(id);
+            if (webApiTab == null)
+            {
+                return NotFound();
+            }
+            dbobj.WebApiTabs.Remove(webApiTab);
+            dbobj.SaveChanges();
+            return Ok(webApiTab);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCAPIPROJECT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MVCAPIPROJECT")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da0e8029-a3d6-447a-b5a3-9b5d8dd46967")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
